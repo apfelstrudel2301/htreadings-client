92c6ec6016781e4c3d31907bb63e00cef0736b74
Christian Degel <degel.christian@gmail.com>
2020-04-03 18:17:30 +0200

    move sensor initialization out of loop

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # htreadings-client
+The client software for recording DHT22 sensor readings with a Raspberry Pi and sending them to an API (e.g. for displaying the data in a dashboard).
+
+## Getting started
+
+### Prerequisites
+- DHT22 sensor connected to Raspberry Pi
+- Raspbian Buster operating system (if you have to upgrade from stretch check out this [upgrade tutorial](https://pimylifeup.com/upgrade-raspbian-stretch-to-raspbian-buster))
+
+### Install dependencies
+
+Install the Python dependencies
+```bash
+pip install -r requirements.txt
+```
+
+Installl the libgiod2 library needed by the [CircuitPython Libraries](https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/python-setup).
+```bash
+sudo apt-get install libgpiod2
+```
+
+### Configure sensor readings script
+Configure the following attributes in [htreadings-client.py](htreadings-client.py):
+- API endpoint
+- API_KEY
+- d
+
+### Start script
+Start recording sensor readings and sending them to an API
+
+- Install Adafruit package from 
+- Configure gpio of the DHT22 sensor in `htreadings-client.py`
+
+
+```bash
+```
--- htreadings-client.py
+++ htreadings-client.py
@@ -11,20 +11,61 @@ INTERVAL = 300
 DB_PATH = 'db/sensordata.db'
 API_BASE_URL = 'https://eatpcfzrgg.execute-api.eu-central-1.amazonaws.com/int'
 API_KEY = '1iXFG6qHJZ6C2CD1Ihvhv9s1tA2UYaXaDVZs9114'
-GPIO = 4
+# GPIO = 4
 MOCK_SENSOR_READINGS = False
 
 
-def get_sensor_reading(gpio, db_path):
+def main():
+    api_url_single = API_BASE_URL + '/htreadings-single'
+    api_url_bulk = API_BASE_URL + '/htreadings-bulk'
+    headers = {
+        'x-api-key': API_KEY,
+        'Content-Type': 'application/json'
+    }
+    iteration_count = 0
+    # Used in case of errors to recover the delta between the local and remote data
+    push_latest_db_entries = True
+    dht_device = adafruit_dht.DHT22(board.D4)
+    while True:
+        timestamp, temperature, humidity = get_sensor_reading(dht_device=dht_device, db_path=DB_PATH)
+        if push_latest_db_entries:
+            print('Push latest database entries')
+            conn = sqlite3.connect(DB_PATH)
+            cursor = conn.cursor()
+            cursor.execute(
+                'SELECT * FROM (SELECT * FROM htreadings ORDER BY timestamp desc LIMIT 201) ORDER BY timestamp asc')
+            rows = cursor.fetchall()
+            conn.close()
+            entries_dict_list = [dict(zip(['id', 'timestamp', 'temperature', 'humidity'], values)) for values in
+                                 rows]
+            bulk_upload_successful = upload(api_url=api_url_bulk, headers=headers, data=entries_dict_list)
+            if bulk_upload_successful:
+                print('Successfully pushed history with ' + str(len(entries_dict_list)) + ' values')
+                push_latest_db_entries = False
+            else:
+                print('Bulk upload failed.')
+        else:
+            data = {
+                'timestamp': datetime.datetime.strftime(timestamp, "%Y-%m-%d %H:%M:%S.%f"),
+                'temperature': temperature,
+                'humidity': humidity
+            }
+            single_upload_successful = upload(api_url=api_url_single, headers=headers, data=data)
+            if single_upload_successful:
+                iteration_count += 1
+                print('Sent values ' + str(iteration_count))
+            else:
+                print('Single upload failed.')
+                push_latest_db_entries = True
+        print('Iteration complete')
+        time.sleep(INTERVAL)
+
+
+def get_sensor_reading(dht_device, db_path):
     if not MOCK_SENSOR_READINGS:
-        # import Adafruit_DHT
-        # sensor = Adafruit_DHT.DHT22
-        # humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
-        temperature = humidity = None
-        while not (temperature and humidity):
+        temperature = humidity = timestamp = None
+        while not (temperature and humidity and timestamp):
             try:
-                dht_device = adafruit_dht.DHT22(board.D4)
-                print('initialized dht_device')
                 temperature = dht_device.temperature
                 print(temperature)
                 humidity = dht_device.humidity
@@ -46,64 +87,12 @@ def get_sensor_reading(gpio, db_path):
                 print('Error reading the sensor')
                 print(error.args[0])
                 print('-------------------------------------------------')
-            time.sleep(5)
+            time.sleep(2)
     else:
         timestamp = datetime.datetime.now()
         temperature = 23.4
         humidity = 56.7
     return timestamp, temperature, humidity
-
-
-def main():
-    api_url_single = API_BASE_URL + '/htreadings-single'
-    api_url_bulk = API_BASE_URL + '/htreadings-bulk'
-    headers = {
-        'x-api-key': API_KEY,
-        'Content-Type': 'application/json'
-    }
-    iteration_count = 0
-    # Used in case of errors to recover the delta between the local and remote data
-    push_latest_db_entries = True
-    while True:
-        try:
-            timestamp, temperature, humidity = get_sensor_reading(gpio=GPIO, db_path=DB_PATH)
-            recording_error = False
-        except Exception as e:
-            print('Recording error')
-            print(e)
-            recording_error = True
-        if not recording_error:
-            if push_latest_db_entries:
-                print('Push latest database entries')
-                conn = sqlite3.connect(DB_PATH)
-                cursor = conn.cursor()
-                cursor.execute(
-                    'SELECT * FROM (SELECT * FROM htreadings ORDER BY timestamp desc LIMIT 201) ORDER BY timestamp asc')
-                rows = cursor.fetchall()
-                conn.close()
-                entries_dict_list = [dict(zip(['id', 'timestamp', 'temperature', 'humidity'], values)) for values in
-                                     rows]
-                bulk_upload_successful = upload(api_url=api_url_bulk, headers=headers, data=entries_dict_list)
-                if bulk_upload_successful:
-                    print('Successfully pushed history with ' + str(len(entries_dict_list)) + ' values')
-                    push_latest_db_entries = False
-                else:
-                    print('Bulk upload failed.')
-            else:
-                data = {
-                    'timestamp': datetime.datetime.strftime(timestamp, "%Y-%m-%d %H:%M:%S.%f"),
-                    'temperature': temperature,
-                    'humidity': humidity
-                }
-                single_upload_successful = upload(api_url=api_url_single, headers=headers, data=data)
-                if single_upload_successful:
-                    iteration_count += 1
-                    print('Sent values ' + str(iteration_count))
-                else:
-                    print('Single upload failed.')
-                    push_latest_db_entries = True
-        print('Iteration complete')
-        time.sleep(INTERVAL)
 
 
 def upload(api_url, headers, data):
